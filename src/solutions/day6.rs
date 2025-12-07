e906c3fa5a42406c63d71912e2e8fe072949c0da
Gergely Szabo <gege251@mailbox.org>
2025-12-07 07:12:04 +0100

    Day 6 part 2

--- src/solutions/day6.rs
+++ src/solutions/day6.rs
@@ -5,31 +5,60 @@ use std::{
 
 use anyhow::{Context, anyhow};
 
-pub fn solution(matrix: &[Vec<String>]) -> anyhow::Result<u64> {
+#[derive(Debug, Clone, Copy)]
+enum Op {
+    Sum,
+    Prod,
+}
+
+pub fn solution(mut lines: Vec<String>) -> anyhow::Result<u64> {
     let mut total = 0u64;
-    let last_row = matrix.last().ok_or(anyhow!(
+
+    let last_row = lines.split_off(lines.len() - 1).pop().ok_or(anyhow!(
         "there must be at least one input row with operators"
     ))?;
-    for (i, op) in last_row.iter().enumerate() {
-        let op_identity = match &op[..] {
-            "*" => Ok::<u64, anyhow::Error>(1),
-            "+" => Ok(0),
-            _ => Err(anyhow!("unknown operator {op}"))?,
-        };
-        let op = |x, y| match &op[..] {
-            "*" => Ok::<u64, anyhow::Error>(x * y),
-            "+" => Ok(x + y),
-            _ => Err(anyhow!("unknown operator {op}"))?,
+
+    let mut op = Op::Sum;
+    let mut operands = Vec::new();
+
+    let last_row = last_row.chars().enumerate();
+    for (i, op_char) in last_row {
+        match op_char {
+            '*' => op = Op::Prod,
+            '+' => op = Op::Sum,
+            ' ' => {}
+            _ => Err(anyhow!("unknown operator {op:?}"))?,
         };
 
-        total += matrix
-            .iter()
-            .take(matrix.len() - 1)
-            .try_fold(op_identity?, |result, xs| {
-                let x = xs[i].parse::<u64>().context("couldn't parse number")?;
-                op(x, result)
+        let column = lines
+            .iter_mut()
+            .try_fold(String::new(), |mut result, chars| {
+                let next = chars
+                    .as_bytes()
+                    .get(i)
+                    .context("there must be exactly equivalent length rows")?;
+                result.push(*next as char);
+                Ok::<String, anyhow::Error>(result)
             })?;
+
+        let column = column.trim();
+
+        if column.is_empty() {
+            total += match op {
+                Op::Prod => operands.iter().product::<u64>(),
+                Op::Sum => operands.iter().sum(),
+            };
+            operands.clear();
+        } else {
+            operands.push(column.parse::<u64>().context("parsing operand")?);
+        }
     }
+
+    total += match op {
+        Op::Prod => operands.iter().product::<u64>(),
+        Op::Sum => operands.iter().sum(),
+    };
+
     Ok(total)
 }
 
@@ -37,19 +66,12 @@ pub fn run() -> anyhow::Result<()> {
     let f = File::open("inputs/day6").context("couldn't open day 6 file")?;
     let buf = BufReader::new(f);
 
-    let matrix = buf
+    let lines = buf
         .lines()
-        .map(|line| {
-            let line = line.context("couldn't read line")?;
-            let words = line
-                .split_whitespace()
-                .map(String::from)
-                .collect::<Vec<String>>();
-            Ok(words)
-        })
+        .map(|line| line.context("couldn't read line"))
         .collect::<anyhow::Result<Vec<_>>>()?;
 
-    let total = solution(&matrix)?;
+    let total = solution(lines)?;
 
     eprintln!("Day 6: {total}");
 
@@ -63,20 +85,14 @@ mod test {
     #[test]
     fn test() {
         let testdata = [
-            ["123", "328", "51", "64"],
-            ["45", "64", "387", "23"],
-            ["6", "98", "215", "314"],
-            ["*", "+", "*", "+"],
+            "123 328  51 64 ",
+            " 45 64  387 23 ",
+            "  6 98  215 314",
+            "*   +   *   +  ",
         ];
 
-        let sum = solution(
-            &testdata
-                .into_iter()
-                .map(|xs| xs.into_iter().map(String::from).collect::<Vec<_>>())
-                .collect::<Vec<_>>(),
-        )
-        .unwrap();
+        let sum = solution(testdata.into_iter().map(String::from).collect::<Vec<_>>()).unwrap();
 
-        assert_eq!(sum, 4277556);
+        assert_eq!(sum, 3263827);
     }
 }
